e57d51dd8fb4b30fd0f045ed6415b25e0d9ed86e
Warunyou Kongchai <photboatxeemus@icloud.com>
2021-06-15 18:08:20 +0700

    demo fetch image from URL

--- DemoFetchImageURL/ViewController.swift
+++ DemoFetchImageURL/ViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let urlString = "https://cdn.arstechnica.net/wp-content/uploads/2018/06/macOS-Mojave-Dynamic-Wallpaper-transition.jpg"
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        imageView.backgroundColor = .red
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(imageView)
+        imageView.center = view.center
+        
+        fetchImage()
+    }
+    
+    private func fetchImage() {
+        // get data
+        // convertthe data to image
+        // set image to imageview
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let getDataTask = URLSession.shared.dataTask(with: url){ data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self.imageView.image = image
+            }
+        }
+        
+        getDataTask.resume()
     }
 
 
---
+++ Pods/Target Support Files/Pods-DemoFetchImageURL/Pods-DemoFetchImageURL-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoFetchImageURL : NSObject
+@end
+@implementation PodsDummy_Pods_DemoFetchImageURL
+@end
